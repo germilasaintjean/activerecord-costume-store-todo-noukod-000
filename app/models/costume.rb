4401ede028bf6777075e52a8804f1c784409c0dd
Saint-Jean Germila <germila@noukod.com>
2018-12-05 12:14:46 +0000

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,6 +1,5 @@
 class Costume < ActiveRecord::Base
 
-  attr_accesssor :name, :price, :image_url, :size
 end
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,14 @@
-class CostumeStore < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
+
+      t.timestamps null: false
+    end
+  end
 end
 # Create a class and inherit from ActiveRecord::Migration
 
